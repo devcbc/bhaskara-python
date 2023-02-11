cdc35ef20d2b3428d59e57aba46d5e252ae94ae6
Cauã <124628936+devcbc@users.noreply.github.com>
2023-02-11 17:03:43 -0300

    Add files via upload

---
+++ CALCULOBHASKARA-1.py
@@ -0,0 +1,12 @@
+import math
+
+def bhaskara(a, b, c):
+    delta = b ** 2 - 4 * a * c
+    if delta < 0:
+        return None
+    else:
+        x1 = (-b + math.sqrt(delta)) / (2 * a)
+        x2 = (-b - math.sqrt(delta)) / (2 * a)
+        return x1, x2
+
+    
